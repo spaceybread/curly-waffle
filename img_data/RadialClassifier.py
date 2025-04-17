b7e323ed9ff636c24dfc882cf82a578317cc8fa0
spaceybread <spaceybread@gmail.com>
2025-04-17 13:37:06 -0500

    it would be nice if i actually used git

--- img_data/RadialClassifier.py
+++ img_data/RadialClassifier.py
@@ -5,13 +5,31 @@ import numpy as np
 import sys
 
 class RadialClassifier:
-    def __init__(self, reg, rad = -1):
+    def __init__(self, reg, rad = -1, mit = 0):
         self.center = reg.mean(axis=0)
-        self.radius = rad if rad != -1 else self.get_def_rad(reg)
+        
+        self.lo = self.get_def_rad(reg, mi = mit)
+        self.hi = self.get_def_rad(reg, mi = mit) * 2
+        self.radius = self.compute_opt_r()
+        
+    def get_def_rad(self, reg, mi = 0):
+        # print([np.linalg.norm(self.center - x) for x in reg])
+        if mi == 0: return max([np.linalg.norm(self.center - x) for x in reg])
+        return min([np.linalg.norm(self.center - x) for x in reg])
     
-    def get_def_rad(self, reg):
-        print([np.linalg.norm(self.center - x) for x in reg])
-        return max([np.linalg.norm(self.center - x) for x in reg])
+    def compute_opt_r(self):
+        return (self.hi + self.lo) / 2
+        
+    def set_opt_rad(self, M = 0):
+        if M == 0: return self.compute_opt_r()
+        
+        if M == 1:
+            self.lo = self.radius
+            self.radius = self.compute_opt_r()
+        
+        if M == -1:
+            self.hi = self.radius
+            self.radius = self.compute_opt_r()
         
     def resolve(self, candidate):
         return np.linalg.norm(self.center - candidate) <= self.radius
---
+++ img_data/adj_rad.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+from RadialClassifier import *
+from sklearn import metrics
+from loader import *
+import pandas as pd
+import numpy as np
+import sys
+
+SAMPLES = 100
+T = 10
+daf, ids = load_data(sys.argv[1], sys.argv[2])
+
+test_class = sample_group_name(ids)
+t_l, f_l = [], []
+
+for i in range(SAMPLES):
+    t, f = sample_tp(ids, test_class)
+    t_l.append(t)
+    f_l.append(f)
+
+results = []
+results1 = []
+
+delta = 0.1
+start = -1
+final = -1
+
+start1 = -1
+final1 = -1
+for i in range(100):
+    reg_vals, _, _ = sample_ids_from_group(ids, test_class, T)
+    r, tv, fv = get_vectors(reg_vals, t_l, f_l, daf)
+    r = np.array([r.iloc[i].to_numpy() for i in range(T)])
+    print(r)
+    
+    # print(test_class)
+    # print(tv)
+    RC = RadialClassifier(r)
+    RC1 = RadialClassifier(r, mit = 1)
+    if start == -1: start = RC.get_radius()
+    if start1 == -1: start1 = RC1.get_radius()
+    
+    TMR = sum([1 if RC.resolve(tv.iloc[i].to_numpy()) else 0 for i in range(SAMPLES)])
+    FMR = sum([1 if RC.resolve(fv.iloc[i].to_numpy()) else 0 for i in range(SAMPLES)])
+    TMR, FMR = TMR / SAMPLES, FMR / SAMPLES
+    
+    if FMR > delta: RC.set_opt_rad(-1)
+    else: RC.set_opt_rad(1)
+    
+    results.append((RC.get_radius(), TMR, FMR))
+    final = RC.get_radius()
+    
+    TMR = sum([1 if RC1.resolve(tv.iloc[i].to_numpy()) else 0 for i in range(SAMPLES)])
+    FMR = sum([1 if RC1.resolve(fv.iloc[i].to_numpy()) else 0 for i in range(SAMPLES)])
+    TMR, FMR = TMR / SAMPLES, FMR / SAMPLES
+    
+    if FMR > delta: RC1.set_opt_rad(-1)
+    else: RC1.set_opt_rad(1)
+    
+    results1.append((RC1.get_radius(), TMR, FMR))
+    final1 = RC1.get_radius()
+
+results.sort()
+results1.sort()
+# print(results)
+print(start, final)
+print(start1, final1)
+
+
+radius = [x[0] for x in results]
+tmr = [x[1] for x in results]
+fmr = [x[2] for x in results]
+plt.plot(radius, tmr, 'g-', label="TMR - MAX")
+plt.plot(radius, fmr, 'r-', label="FMR - MAX")
+
+radius1 = [x[0] for x in results1]
+tmr1 = [x[1] for x in results1]
+fmr1 = [x[2] for x in results1]
+plt.plot(radius1, tmr1, 'g.', label="TMR - MIN")
+plt.plot(radius1, fmr1, 'r.', label="FMR - MIN")
+
+
+plt.xlabel("Radius")
+plt.ylabel("Rate")
+plt.title("TMR vs. FMR @ Radius")
+plt.legend()
+
+
+plt.show()
