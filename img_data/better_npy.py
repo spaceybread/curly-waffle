7dd4ca0a11ebcb1b630d500a895b68ba2d6b4a50
spaceybread <spaceybread@gmail.com>
2025-05-08 14:11:07 -0500

    data set maker

---
+++ img_data/better_npy.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+import matplotlib.pyplot as plt
+from sklearn import metrics
+import pandas as pd
+import numpy as np
+import random
+import sys
+
+T = 15
+R = 8
+
+def first_load(npz_file, text_file):
+    npz = np.load(npz_file)
+    ids = [x.strip().split('/')[-2] for x in open(text_file)]
+
+    if (len(ids) != npz.shape[0]): print("Size Mismatch")
+    
+    grp = defaultdict(list)
+    for s, v in zip(ids, npz): grp[s].append(v)
+
+    rem = []
+    for k in grp.keys():
+        if len(grp[k]) < T: rem.append(k)
+    
+    for r in rem: grp.pop(r)
+    np.save('mapped_data.npy', grp)
+    
+
+def test_set(npz_file):
+    data = np.load(npz_file, allow_pickle=True).item()
+    all_keys = list(data.keys())
+    ma = {}
+    
+    for x in data.keys():
+        reg_vec = random.choices(data[x], k = R)
+        in_set = random.choices(data[x], k = T)
+        out_set = []
+        out_key = x
+        for i in range(T):
+            while out_key != x:
+                out_key = random.choices(all_keys, k = 1)
+            out_set.append(random.choices(data[out_key], k = 1))
+        
+        center = np.mean(reg_vec, axis=0)
+        distances = np.linalg.norm(reg_vec - center, axis=1)
+        init_rad = np.max(distances)
+        
+        ma[x] = (init_rad, center, in_set, out_set)
+        # print(ma[x][0], ma[x][1])
+    
+    np.save('accum_data.npy', ma)
+
+def load_accum(npz_file):
+    data = np.load(npz_file, allow_pickle=True).item()
+    
+    # example
+    print(list(data.keys())[2])
+    print(data['8766'])
+    
+    
+first_load(sys.argv[1], sys.argv[2])
+test_set('mapped_data.npy')
+load_accum('accum_data.npy')
