1c32920d00be0fd6be9fffba4f00e6845d3a2e8a
spaceybread <spaceybread@gmail.com>
2025-05-10 18:27:13 -0500

    added more results

--- img_data/over_many.py
+++ img_data/over_many.py
@@ -10,11 +10,10 @@ def get_data(npz_file = 'accum_data_dist.npy'):
     return np.load(npz_file, allow_pickle=True).item()
 
 
-def bin_search(data):
+def bin_search(data, ALPHA = 2/3):
     DELTA = 2
     hi, lo = DELTA, 0
-    ALPHA = 2/3
-    
+     
     # lower bounded TMR
     # x/y result: coefficient [TMR, FMR]
     # 2/3 result: 0.940940409898758 [0.6666846603688709, 0.0030409356725146198]
@@ -24,7 +23,7 @@ def bin_search(data):
     keys = list(data.keys())
     res = {}
     
-    for _ in range(64):
+    for _ in range(32):
         tchk, fchk = 0, 0
         tks, fks = 0, 0
         for key in keys:
@@ -45,8 +44,28 @@ def bin_search(data):
 
     return res, coeff
 
-ma, val = bin_search(get_data())
-print(val)
+# ma, val = bin_search(get_data())
+# print(val)
 
-for x in ma:
-    print(x, ma[x])
+
+DATA = get_data()
+TMR, FMR = [], []
+COEFFS = []
+for i in range(1, 102, 1):
+    resdb, idx = bin_search(DATA, ALPHA = i / 100)
+    COEFFS.append(idx)
+    TMR.append(resdb[idx][0])
+    FMR.append(resdb[idx][1])
+
+plt.figure(figsize=(8, 6))
+plt.plot(FMR, TMR, color='darkorange', lw=2)
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('FMR')
+plt.ylabel('TMR')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
+
+
