8d47d3f52585b574a54410577260f1c92a7e5125
spaceybread <spaceybread@gmail.com>
2025-05-18 11:56:30 +0530

    || binary

--- img_data/lattice_over_many.py
+++ img_data/lattice_over_many.py
@@ -1,3 +1,6 @@
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from collections import Counter
+from functools import partial
 from tqdm import tqdm
 import numpy as np
 import subprocess
@@ -7,13 +10,21 @@ def get_data(npz_file = 'accum_data_for_lattice.npy'):
     return np.load(npz_file, allow_pickle=True).item()
 
 def call_check(scale, a, b):
-    a_str, b_str = vec_to_string(a), vec_to_string(b)
+    a_str = vec_to_string(a)
+    b_str = vec_to_string(b)
     
     EXEC = "./lattice_fuzzy"
     args = [EXEC, str(scale), a_str, b_str]
     result = subprocess.run(args, capture_output=True, text=True)
     
     match = result.stdout.strip()
+    if match != "match!" and match != "no match!":
+        print(result.stdout + "\n\n" + result.stderr)
+        print(a)
+        print(b)
+        print(scale)
+        quit()
+
     return match == "match!"
 
 def vec_to_string(vec):
@@ -24,12 +35,24 @@ def vec_to_string(vec):
 def data_sanity_parsing(data):
     keys = list(data.keys())
     key = keys[0]
-
-#    print(vec_to_string(data[key][3][0]))
     
-    print(data[key][1])
+    cen = data[key][0]
+    rad = data[key][1]
 
-    print(call_check(data[key][1], data[key][0], data[key][2][0]))
+    
+    task = partial(call_check, rad * 5, cen)
+    tchk = 0
+    fchk = 0
+    with ProcessPoolExecutor() as exec:
+        fut = [exec.submit(task, val) for val in t_v]
+        for f in as_completed(fut):
+            if f.result(): tchk += 1
+    with ProcessPoolExecutor() as exec:
+        fut = [exec.submit(task, val) for val in f_v]
+        for f in as_completed(fut):
+            if f.result(): tchk += 1
+    
+    print(tchk, fchk)
     
     
 def bin_search(data, ALPHA = 2/3):
@@ -38,40 +61,51 @@ def bin_search(data, ALPHA = 2/3):
      
     keys = list(data.keys())
     res = {}
-    
-    for _ in tqdm(range(32)):
+    exec = ProcessPoolExecutor()
+    for _ in tqdm(range(12)):
         tchk, fchk = 0, 0
         tks, fks = 0, 0
         for key in tqdm(keys):
         
             coeff = (hi + lo) / 2
-            rad = data[key][0] * coeff
-        
-            tchk += sum([1 if call_check(rad, data[key][0], val) else 0 for val in data[key][2]])
-            tks += len(data[key][2])
-            fchk += sum([1 if call_check(rad, data[key][0], val) else 0 for val in data[key][3]])
-            fks += len(data[key][3])
-        
+            rad = data[key][1] * coeff
+            t_v = data[key][2]
+            f_v = data[key][3]
+            cen = data[key][0]
+            
+            task = partial(call_check, rad, cen)
+            
+            t_results = exec.map(task, t_v)
+            f_results = exec.map(task, f_v)
+
+            tchk += sum(t_results)
+            fchk += sum(f_results)
+
+            tks += len(t_v)
+            fks += len(f_v)
+            
         tmr, fmr = tchk / tks, fchk / fks
         res[coeff] = [tmr, fmr]
-        
+        print(coeff, tmr, fmr)
         # do lower bounded TMR not upper bounded FMR
         if tmr > ALPHA: hi = coeff
         else: lo = coeff
-
+        
+    exec.shutdown()
     return res, coeff
-    
+
+if __name__ == '__main__':
+    scale = "1.0"
+    a = "1.0 " * 512
+    b = "1.0 " * 512
 
 
+    data = get_data()
+    out = bin_search(data)
+#    data_sanity_parsing(data)
 
-scale = "1.0"
-a = "1.0 " * 512
-b = "1.0 " * 512
+    np.save('processed_lattice.npy', out)
 
-
-data = get_data()
-out = bin_search(data)
-
-for r in out:
-    print(r, out[r])
+    for r in out.keys():
+        print(r, out[r])
 
